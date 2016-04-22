9c66b110dd57454f79edf64a5cf84ffa789e368c
zz <zhuguang@ejoy.com>
2016-04-22 17:32:18 +0800

    [ADDED] blend

--- shader/BlendShader.h
+++ shader/BlendShader.h
@@ -2,11 +2,16 @@
 #define _SHADERLAB_BLEND_SHADER_H_
 
 #include "Shader.h"
+#include "BlendMode.h"
+#include "ShaderProgram.h"
+#include "render/VertexAttrib.h"
 
 #include <stdint.h>
 
 namespace sl
 {
+
+class ShaderProgram;
 
 class BlendShader : public Shader
 {
@@ -20,12 +25,63 @@ public:
 	virtual void BindTexture();
 
 	void SetColor(uint32_t color, uint32_t additive);
-	void SetColorMap(uint32_t rmap, uint32_t gmap, uint32_t bmap);
 
 	void SetMode(int mode);
 
-	void Draw(const float* positions, const float* texcoords, 
-		const float* texcoords_base, int texid, int texid_base) const;
+	void Draw(const float* positions, const float* texcoords_blend, 
+		const float* texcoords_base, int tex_blend, int tex_base) const;
+
+private:
+	void InitVAList();
+	void InitProg();
+
+private:
+	enum VA_TYPE {
+		POSITION = 0,
+		TEXCOORD,
+		TEXCOORD_BASE,
+		COLOR,
+		ADDITIVE,
+		VA_MAX_COUNT
+	};
+
+	struct Vertex
+	{
+		float vx, vy;
+		float tx_blend, ty_blend;
+		float tx_base, ty_base;
+		uint32_t color, additive;
+	};
+
+	class Program : public ShaderProgram
+	{
+	public:
+		Program(RenderContext* rc, const std::vector<VertexAttrib>& va_list, 
+			RenderBuffer* ib);
+		
+		void SetMode(int mode);
+
+	private:
+		void Init(const std::vector<VertexAttrib>& va_list, RenderBuffer* ib);
+
+	private:
+		int m_mode;
+
+	}; // Program
+
+private:
+	VertexAttrib m_va_list[VA_MAX_COUNT];
+
+	Program* m_prog;
+
+	uint32_t m_color, m_additive;
+	
+	SL_BLEND_MODE m_curr_mode;
+
+	mutable int m_tex_blend, m_tex_base;
+
+	Vertex* m_vertex_buf;
+	mutable int m_quad_sz;
 
 }; // BlendShader
 
